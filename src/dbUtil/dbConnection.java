bab87d099a1f789fe82e72cb431e67f0098b5bd8
namaoy2538 <namaoy999999@gmail.com>
2018-03-04 17:58:22 -0800

    dbConnection

---
+++ src/dbUtil/dbConnection.java
@@ -0,0 +1,36 @@
+package dbUtil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbConnection {
+    private static final String SQCONN = "jdbc:sqlite:school.sqlite";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            return DriverManager.getConnection(SQCONN);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+
+
+
+}//class
+© 2018 GitHub, Inc.
+        Terms
+        Privacy
+        Security
+        Status
+        Help
+        Contact GitHub
+        API
+        Training
+        Shop
+        Blog
